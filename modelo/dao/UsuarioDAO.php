6d77372355a996c6e9b174675ed6dd05a3a7d2cd
Helder <helder.nunes@afogados.ifpe.edu.br>
2024-04-18 21:28:19 -0300

    Aula 18/04

---
+++ modelo/dao/UsuarioDAO.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto32024/modelo/dao/BDPDO.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/projeto32024/modelo/vo/Usuario.php';
+
+class UsuarioDAO {
+
+    public function insert($usuario) {
+        try {
+            $sql = "INSERT INTO usuario (nome,email,login,senha) "
+                    . "VALUES (:nome,:email,:login,:senha)";
+            //perceba que na linha abaixo vai precisar de um import
+            $p_sql = BDPDO::getInstance()->prepare($sql);
+            $p_sql->bindValue(":nome", $usuario->getNome());
+            $p_sql->bindValue(":email", $usuario->getEmail());
+            $p_sql->bindValue(":login", $usuario->getLogin());
+            $p_sql->bindValue(":senha", $usuario->getSenha());
+            return $p_sql->execute();
+        } catch (Exception $e) {
+            print "Erro ao executar a função de salvar" . $e->getMessage();
+        }
+    }
+}
